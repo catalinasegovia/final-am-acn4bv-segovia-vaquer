c3dec5dfb686db1b8e32ced5ec077a84751bd169
Catalina Segovia <91673725+catalinasegovia@users.noreply.github.com>
2023-10-25 14:08:20 -0300

    .

--- app/src/main/java/com/example/parcial1_segovia_vaquer/ActivityJuego1.java
+++ app/src/main/java/com/example/parcial1_segovia_vaquer/ActivityJuego1.java
@@ -7,14 +7,33 @@ import android.widget.Button;
 
 import android.os.Bundle;
 
+import java.util.Random;
+
 public class ActivityJuego1 extends AppCompatActivity  {
 
-
+    int[] idCartas = { R.drawable.carta_2, R.drawable.carta_3, R.drawable.carta_4, R.drawable.carta_5, R.drawable.carta_6, R.drawable.carta_7, R.drawable.carta_8,
+            R.drawable.carta_9, R.drawable.carta_10, R.drawable.carta_q, R.drawable.carta_j, R.drawable.carta_k, R.drawable.carta_a,
+    };
+    int  [] valor= { 2,3,4,5,6,7,8,9,10,10,10,10,11};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_juego1);
+
+        //intent para utilizar los arrays en activity 2 para luego crear las cartas
+        Intent arrayI= new Intent(ActivityJuego1.this, ActivityJuego2.class);
+        arrayI.putExtra("array id cartas" , idCartas);
+        arrayI.putExtra("array valor cartas", valor);
+        startActivity(arrayI);
+
+
+        // intent para utilizar los arrays en activity 3 para luego crear las cartas
+        Intent array2= new Intent(ActivityJuego1.this, ActivityJuego3.class);
+        array2.putExtra("array id cartas" , idCartas);
+        array2.putExtra("array valor cartas", valor);
+        startActivity(array2);
+
 
 
 
@@ -22,9 +41,34 @@ public class ActivityJuego1 extends AppCompatActivity  {
    btn_tirar.setOnClickListener( new View.OnClickListener() {
 
         public void onClick(View view) {
-        Intent intent = new Intent ( getApplicationContext(), ActivityJuego2.class);
-        startActivity(intent);
+
+            int pc1 = cartaRandom();
+
+            int idCarta1= idCartas[pc1];
+            int valorCarta1=valor[pc1];
+
+            Intent intent1 = new Intent ( ActivityJuego1.this, ActivityJuego2.class);
+            intent1.putExtra("posicion", pc1);
+            intent1.putExtra("id imagen", idCarta1);
+            intent1.putExtra("valor carta", valorCarta1);
+
+            startActivity(intent1);
+
+
     }
 
     });
-}}
+
+
+}       public int cartaRandom(){
+        Random c1= new Random();
+
+        int pc1= c1.nextInt(idCartas.length);
+
+        Intent intent1 = new Intent ( getApplicationContext(), ActivityJuego2.class);
+        startActivity(intent1);
+
+        return pc1;
+    }
+
+}
--- app/src/main/java/com/example/parcial1_segovia_vaquer/ActivityJuego2.java
+++ app/src/main/java/com/example/parcial1_segovia_vaquer/ActivityJuego2.java
@@ -2,36 +2,74 @@ package com.example.parcial1_segovia_vaquer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import java.util.Random;
 
-public class ActivityJuego2 extends AppCompatActivity {
+public class ActivityJuego2 extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_juego2);
 
+        int pc1 = getIntent().getIntExtra("", -1);
+        int idCarta1= getIntent().getIntExtra("", -1);
+        int valorCarta1=getIntent().getIntExtra("", -1);
 
-        int[] idCartas = { R.drawable.carta_2, R.drawable.carta_3, R.drawable.carta_4, R.drawable.carta_5, R.drawable.carta_6, R.drawable.carta_7, R.drawable.carta_8,
-        R.drawable.carta_9, R.drawable.carta_10, R.drawable.carta_q, R.drawable.carta_j, R.drawable.carta_k, R.drawable.carta_a,
-        };
-        int  [] valorCarta= { 2,3,4,5,6,7,8,9,10,10,10,10,11};
+        if (pc1 != -1 && idCarta1 != -1 && valorCarta1 != -1){
 
-        Random random = new Random();
-        int cartaTot = random.nextInt(idCartas.length);
-        int cartaRandom = idCartas[cartaTot];
-        int valorCartaRandom= valorCarta[cartaTot];
+            ImageView imagenC1= findViewById(R.id.randomImg);
+            imagenC1.setImageResource(idCarta1);
+
+        }
+
+        Button btn_3= (Button)findViewById(R.id.btn_3);
+
+        btn_3.setOnClickListener( new View.OnClickListener() {
+            public void onClick(View view) {
 
 
-        ImageView randomImg= findViewById(R.id.randomImg);
-        randomImg.setImageResource(cartaRandom);
+                int pc2 = cartaRandom2();
+                int idCarta2= idCartasI[pc2];
+                int valorCarta2=valorI[pc2];
 
+                Intent intent2 = new Intent ( getApplicationContext(), ActivityJuego3.class);
+                intent2.putExtra("posicion", pc2);
+                intent2.putExtra("id imagen", idCarta2);
+                intent2.putExtra("valor carta", valorCarta2);
+
+                startActivity(intent2);
+
+            }
+        });
 
 
 
 
     }
-}
+
+    int[] idCartasI= getIntent().getIntArrayExtra("id cartas");
+    int[] valorI= getIntent().getIntArrayExtra("valor cartas");
+
+    public int cartaRandom2(){
+        Random c2= new Random();
+
+        int pc2= c2.nextInt(idCartasI.length);
+
+        Intent intent2 = new Intent ( getApplicationContext(), ActivityJuego3.class);
+        startActivity(intent2);
+
+        return pc2;
+    }
+
+
+
+    }
+
+
+
