ee43da8968e7d8274713e2fa497ae5ba686d4830
catalinasegovia <catalina.segovia@davinci.edu.ar>
2023-12-01 10:30:06 -0300

    activity 6

--- app/src/main/java/com/example/parcial1_segovia_vaquer/ActivityJuego6.java
+++ app/src/main/java/com/example/parcial1_segovia_vaquer/ActivityJuego6.java
@@ -9,9 +9,14 @@ import android.widget.Button;
 import android.content.Intent;
 import android.view.View;
 
+
 public class ActivityJuego6 extends AppCompatActivity {
     private int puntajeJugador;
     private int puntajeIA;
+    ConstraintLayout layout;
+
+    TextView textView1;
+    TextView textView2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,7 +25,9 @@ public class ActivityJuego6 extends AppCompatActivity {
         puntajeJugador = getIntent().getIntExtra("puntajeJugador",0);
         puntajeIA = getIntent().getIntExtra("puntajeIA",0);
 
-
+        layout = findViewById(R.id.layout);
+        textView1 = findViewById(R.id.textView1);
+        textView2 = findViewById(R.id.textView2);
         resultadoJuego();
 
         Button buttonFin = findViewById(R.id.buttonFin);
@@ -39,27 +46,35 @@ public class ActivityJuego6 extends AppCompatActivity {
 
 
     public void resultadoJuego() {
-        ConstraintLayout layout = findViewById(R.id.layout);
-        layout.setBackgroundResource(R.drawable.portada);
-        TextView textView1 = findViewById(R.id.textView1);
-        TextView textView2 = findViewById(R.id.textView2);
-
-        if (puntajeJugador > puntajeIA) {
-            // El jugador gana
-            layout.setBackgroundResource(R.drawable.ganar);
-
-            textView1.setText("¡Felicidades!");
-            textView2.setText("¡GANASTE!");
-
-        } else if (puntajeIA > puntajeJugador) {
-            // La IA gana
-            layout.setBackgroundResource(R.drawable.perder);
-            textView1.setText("¡Mejor suerte la próxima!");
-            textView2.setText("PERDISTE");
+        if (puntajeJugador > puntajeIA ) {
+            ganoJugador();
+        } else if (puntajeIA > puntajeJugador || puntajeJugador > 21) {
+            perdioJugador();
         } else {
-            // Empate
-            layout.setBackgroundResource(R.drawable.empatar);
-            textView1.setText("¡Mejor suerte la próxima!");
-            textView2.setText("EMPATASTE");
+            empataron();
         }
-    } }
+    }
+
+    public void ganoJugador(){
+        layout.setBackgroundResource(R.drawable.ganar);
+        textView1.setText("¡Felicidades!");
+        textView2.setText("¡GANASTE!");
+
+    }
+
+    public void perdioJugador(){
+        layout.setBackgroundResource(R.drawable.perder);
+        textView1.setText("¡Mejor suerte la próxima!");
+        textView2.setText("PERDISTE");
+
+    }
+
+    public void empataron(){
+        layout.setBackgroundResource(R.drawable.empatar);
+        textView1.setText("¡Mejor suerte la próxima!");
+        textView2.setText("EMPATASTE");
+
+
+    }
+
+}
