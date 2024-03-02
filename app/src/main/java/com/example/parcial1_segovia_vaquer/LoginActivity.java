551872227698cafc44f14365395dfead859b8d7d
catalinasegovia <catalina.segovia@davinci.edu.ar>
2024-03-02 11:55:10 -0300

    registro

--- app/src/main/java/com/example/parcial1_segovia_vaquer/LoginActivity.java
+++ app/src/main/java/com/example/parcial1_segovia_vaquer/LoginActivity.java
@@ -14,23 +14,57 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
 
 public class LoginActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
-    public void login(String email, String password) {
-        Log.i("firebase", "email: " + email);
-        Log.i("firebase", "password: " + password);
-        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-        //startActivity(intent);
+    private FirebaseDatabase database;
+    private DatabaseReference ref;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        ref= database.getReference();
+    }
+
+    public void login(View view) {
+        EditText emailInput = findViewById(R.id.txt_email);
+        EditText passwordInput = findViewById(R.id.txt_password);
+
+        String email = emailInput.getText().toString();
+        String password = passwordInput.getText().toString();
+
         mAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
 
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            Intent intent = new Intent(getApplicationContext(), Activity2.class);
-                            startActivity(intent);
+                            FirebaseUser user = mAuth.getCurrentUsar();
+                            if(user != null){
+                                String userId = user.GetUid();
+                                ref.child("usuario").child(userId).child("email").addListenerForSingleValueEvent(new ValueEventListener(){
+                                    @Override
+                                    public void onDataChange(DataSnapshot dataSnapshot){
+                                        if(dataSnapshot.exists()){
+                                            Intent intent = new Intent(getApplicationContext(), Activity2.class);
+                                            startActivity(intent);
+                                        } else{
+                                            Toast.makeText(LoginActivity.THIS, "No existe usuario con este correo", Toast.LENGTH_SHORT).show();
+                                        }
+                            }
+
+                            @Override
+                                    public void onCancelled(@NonNull DatabaseError databaseError){
+                                        //errror
+                                    }
+
+                            });
+                            }
                         } else {
                             Toast.makeText(LoginActivity.this, "Falló el login",
                                     Toast.LENGTH_SHORT).show();
@@ -42,23 +76,26 @@ public class LoginActivity extends AppCompatActivity {
 
 
     public void onLoginButtonClick(View view){
-        EditText emailImput = findViewById(R.id.txt_email);
-        EditText passwordImput = findViewById(R.id.txt_password);
-
-        String email = emailImput.getText().toString();
-        String password = passwordImput.getText().toString();
-
-        this.login( email, password);
+        login(view);
     }
 
     public void onRegistroButtonClick(View view) {
         Intent intent = new Intent(this, RegistroActivity.class);
         startActivity(intent);
     }
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        mAuth = FirebaseAuth.getInstance();
-    }
+
+
 }
+
+//EditText emailImput = findViewById(R.id.txt_email);
+//        EditText passwordImput = findViewById(R.id.txt_password);
+//
+//        String email = emailImput.getText().toString();
+//        String password = passwordImput.getText().toString();
+//
+//        this.
+//public void login(String email, String password) {
+//        Log.i("firebase", "email: " + email);
+//        Log.i("firebase", "password: " + password);
+//        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+//        //startActivity(intent);
--- app/src/main/java/com/example/parcial1_segovia_vaquer/RegistroActivity.java
+++ app/src/main/java/com/example/parcial1_segovia_vaquer/RegistroActivity.java
@@ -15,22 +15,27 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-//import com.google.firebase.database.DatabaseReference;
-
-
-
-
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class RegistroActivity extends AppCompatActivity {
 
-
     private FirebaseAuth mAuth;
-    private DatabaseReference mDatabase;
+    private FirebaseDatabase database;
+    private DatabaseReference ref;
 
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        ref= database.getReference();
+    }
 
     public void registro(View view) {
+
         EditText emailInput = findViewById(R.id.txt_email);
         EditText passwordInput = findViewById(R.id.txt_password);
         EditText nameInput = findViewById(R.id.txt_name);
@@ -44,18 +49,17 @@ public class RegistroActivity extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            // Guardar el nombre del usuario en la base de datos
+
                             FirebaseUser user = mAuth.getCurrentUser();
-                            String userId = user.getUid();
-                            mDatabase.child("users").child(userId).child("name").setValue(name);
-
-
-
+                            if (user != null){
+                                String userId = user.getUid();
+                                ref.child(userId).child(userId).child("email").setValue(email);
+                            }
                             //Mensaje de confirmacion
                             Log.d("Registro", "Registro exitoso");
                             Toast.makeText(RegistroActivity.this, "Usuario creado exitosamente",
                                     Toast.LENGTH_SHORT).show();
-                            Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                            Intent intent = new Intent(RegistroActivity.this, LoginActivity.class);
                             startActivity(intent);
                         } else {
                             //Mensaje de error
@@ -66,13 +70,4 @@ public class RegistroActivity extends AppCompatActivity {
                     }
                 });
     }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        mAuth = FirebaseAuth.getInstance();
     }
-
-
-}
